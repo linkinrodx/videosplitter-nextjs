5e66affb2d69c82e00ef9bf6712c3eeb30037316
GitHub Action <action@github.com>
2026-03-04 03:54:48 +0000

    chore: auto-increment version

--- lib/version.ts
+++ lib/version.ts
@@ -1,3 +1,3 @@
 // Version is auto-incremented by GitHub Actions workflow
 // Do not edit manually - managed by .github/workflows/auto-increment-version.yml
-export const APP_VERSION = "1.0.1"
+export const APP_VERSION = "1.0.2"
