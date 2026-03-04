0b25ee88ba901d33f326bbe25d137332709d2d08
Rodrigo <rodrigo.saraya@newmarketinc.com>
2026-03-03 21:54:48 -0500

    fix: correct package.json import in version utility

--- lib/version.ts
+++ lib/version.ts
@@ -1,3 +1,8 @@
-const packageJson = require('../../package.json')
+import fs from 'fs'
+import path from 'path'
+
+const packageJson = JSON.parse(
+  fs.readFileSync(path.join(process.cwd(), 'package.json'), 'utf-8')
+)
 
 export const APP_VERSION = packageJson.version
